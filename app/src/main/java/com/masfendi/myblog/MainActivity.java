4d92d155234c760f8022d99f22361c9a675a66a0
nurefendi <nur.efendi@students.amikom.ac.id>
2017-04-30 08:47:42 +0700

    Add Regiser Field

--- app/src/main/java/com/masfendi/myblog/MainActivity.java
+++ app/src/main/java/com/masfendi/myblog/MainActivity.java
@@ -2,6 +2,7 @@ package com.masfendi.myblog;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,6 +15,7 @@ import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseIndexListAdapter;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
@@ -24,10 +26,27 @@ public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if (firebaseAuth.getCurrentUser() == null){
+                    Intent loginIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                    loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(loginIntent);
+                }
+
+            }
+        };
 
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Blog");
 
@@ -39,6 +58,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListener);
 
         FirebaseRecyclerAdapter<blog, BlogViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<blog, BlogViewHolder>(
 
@@ -98,6 +119,13 @@ public class MainActivity extends AppCompatActivity {
         if(item.getItemId() == R.id.add){
             startActivity(new Intent(MainActivity.this, PostActivity.class));
         }
+        if (item.getItemId() == R.id.logoutAction) {
+            logOut();
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void logOut() {
+        mAuth.signOut();
     }
 }
